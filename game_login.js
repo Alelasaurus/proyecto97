dda8418c26556c9225e8dde442f1a827f8cdfae8
Alelasaurus <121989516+Alelasaurus@users.noreply.github.com>
2023-04-30 16:31:27 -0600

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,14 @@
+// Crea la función addUser()
+function addUser() {
+  // Obtén el valor del usuario con id player1_name_input y player2_name_input
+  player1_name=document.getElementById("player1_name_input").value;
+  player2_name=document.getElementById("player2_name_input").value;
+
+  // Almacena los valores en el almacenamiento local
+  localStorage.setItem("player1_name",player1_name);
+  localStorage.setItem("player2_name",player2_name);
+
+  //Asigna "game_page.html" a window.location
+  window.location.replace("game_page.html")
+}
+
